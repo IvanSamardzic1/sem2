94b27c6bef19c137e8398ad4c21d43db21460078
IvanSamardzic1 <115147601+IvanSamardzic1@users.noreply.github.com>
2024-05-10 17:45:55 +0200

    Add files via upload

--- src/fetchanje.js
+++ src/fetchanje.js
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from 'react';
 import { Bar } from 'react-chartjs-2';
 import { Pie } from 'react-chartjs-2';
-import { Line } from 'react-chartjs-2';
 import './tablicaoriginal.css';
 
 const AKDComponent = () => {
